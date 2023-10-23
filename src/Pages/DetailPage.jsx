02476807ec173faa11a1b3546f896ecee582c9b1
Alejandro Spitale <87088490+Wizzard19@users.noreply.github.com>
2023-10-23 18:23:43 -0300

    Update DetailPage.jsx
    commit test

--- src/Pages/DetailPage.jsx
+++ src/Pages/DetailPage.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function DetailPage() {
-	return <div>DetailPage</div>;
+	return (
+		<div>
+			<h2>esto va a ser detail</h2>
+		</div>
+	);
 }
 
 export default DetailPage;
